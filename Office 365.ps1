d5af931f96fcf77b243d66e5c84de449729a1502
perix90 <pedro.gordillo.rios@icloud.com>
2022-08-10 19:23:41 +0200

    Update Office 365.ps1

--- Office 365.ps1
+++ Office 365.ps1
@@ -9,19 +9,20 @@ Write-Host("WELCOME TO OFFICE 365 MANAGEMENT V0.1 BY PEDRO GORDILLO RIOS `n") -F
 
 do{
 
+cls
+
 Write-Host("Select one of the following options: `n") -ForegroundColor Green
 
 Write-Host("1.- Exchange") -ForegroundColor Yellow
 Write-Host("2.- Teams") -ForegroundColor Yellow
 Write-Host("3.- Sharepoint") -ForegroundColor Yellow
-Write-Host("0.- Exit") -ForegroundColor Yellow
-
-$choiceMain = Read-Host -Prompt "`nSelect one of the options above"
 
 
-switch ( $choiceMain ){
+
+switch ( Read-Host -Prompt "`nSelect one of the options above" ){
 
     ######## EXCHANGE
+    
     1 {
 
     cls
@@ -29,7 +30,8 @@ switch ( $choiceMain ){
    
     Write-Host("Select one of the following options: `n") -ForegroundColor Green
 
-    Write-Host("1.- TASK1") -ForegroundColor Yellow
+    Write-Host("1.- Check if user has a mailbox in EU tenant.") -ForegroundColor Yellow
+    Write-Host("2.- Check User Licenses.") -ForegroundColor Yellow
   
     $choiceExchange = Read-Host -Prompt "`nSelect one of the options above"
 
@@ -37,19 +39,29 @@ switch ( $choiceMain ){
     
         1 {
 
+        Connect-ExchangeOnline -UserPrincipalName Pedro.gordillo@rackspaceeu.onmicrosoft.com -DelegatedOrganization hlag.onmicrosoft.com
+        $User = Read-Host -Prompt "`nType user to search for"
+        Get-EXOMailbox -Identity $User -ResultSize unlimited
 
-
-
-     
-
+        break
         }
-    }
+
+        2 {
+
+      
+
+        break
+        }
 
     }
+
+    } 
+    
+    ######## END EXCHANGE
 
     ######## TEAMS
 
-     2 {
+    2 {
 
      cls
      Write-Host("------ TEAMS---- `n") -ForegroundColor Green
@@ -71,12 +83,13 @@ switch ( $choiceMain ){
 
 
 
-     } ##### END TEAMS
+     } 
+     
+    ##### END TEAMS
+    
+    ######## SHAREPOINT
 
-
-  ######## SHAREPOINT
-
-     3 {
+    3 {
 
      cls
     Write-Host("------ SHAREPOINT---- `n") -ForegroundColor Green
@@ -103,16 +116,28 @@ switch ( $choiceMain ){
 
 
      }
+
+
+    ######## END SHAREPOINT
+
 }
 
 
 
-#Disconnect-ExchangeOnline -Confirm:$false -InformationAction Ignore -ErrorAction SilentlyContinue
 
-}while($choiceMain = 0)
+
+}while((Read-Host -Prompt "`nPress 0 to exit or anything else to start over") -ne 0)
+
+Disconnect-ExchangeOnline -Confirm:$false -InformationAction Ignore -ErrorAction SilentlyContinue
+
 
 cls
 Write-Host("Thank you for using this script :) ") -ForegroundColor Green
+
+
+
+
+
 
 
 
