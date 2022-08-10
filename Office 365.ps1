1142a6554374fa60f9477f408864dec67d29584c
perix90 <pedro.gordillo.rios@icloud.com>
2022-08-10 18:42:15 +0200

    Update Office 365.ps1

--- Office 365.ps1
+++ Office 365.ps1
@@ -3,62 +3,106 @@
 
 
 cls
-Write-Host("WELCOME TO OFFICE 365 MANAGEMENT V0.1 BY PEDRO GORDILLO RIOS") -ForegroundColor Green
+Write-Host("WELCOME TO OFFICE 365 MANAGEMENT V0.1 BY PEDRO GORDILLO RIOS `n") -ForegroundColor Green
 
 
 
 do{
 
-
-
-
-
-Write-Host("")
-Write-Host("Select one of the following options:") -ForegroundColor Green
-Write-Host("")
+Write-Host("Select one of the following options: `n") -ForegroundColor Green
 
 Write-Host("1.- Exchange") -ForegroundColor Yellow
 Write-Host("2.- Teams") -ForegroundColor Yellow
 Write-Host("3.- Sharepoint") -ForegroundColor Yellow
 Write-Host("0.- Exit") -ForegroundColor Yellow
 
+$choiceMain = Read-Host -Prompt "`nSelect one of the options above"
 
 
-Write-Host("")
-$choiceMain = Read-Host -Prompt "Select one of the options above"
+switch ( $choiceMain ){
 
-
-switch ( $choiceMain )
-{
+    ######## EXCHANGE
     1 {
 
     cls
-    Write-Host("------ EXCHANGE----") -ForegroundColor Green
-    Write-Host("")
-    Write-Host("Select one of the following options:") -ForegroundColor Green
-    Write-Host("")
+    Write-Host("------ EXCHANGE---- `n") -ForegroundColor Green
+   
+    Write-Host("Select one of the following options: `n") -ForegroundColor Green
 
     Write-Host("1.- TASK1") -ForegroundColor Yellow
   
-    Write-Host("")
-    $choiceExchange = Read-Host -Prompt "Select one of the options above"
+    $choiceExchange = Read-Host -Prompt "`nSelect one of the options above"
 
     switch ( $choiceExchange ) {
     
-    1 {
+        1 {
 
 
 
 
      
 
+        }
     }
-}
+
+    }
+
+    ######## TEAMS
+
+     2 {
+
+     cls
+     Write-Host("------ TEAMS---- `n") -ForegroundColor Green
+     Write-Host("Select one of the following options: `n") -ForegroundColor Green
+     Write-Host("1.- TASK1") -ForegroundColor Yellow
+  
+    $choiceTeams = Read-Host -Prompt "`nSelect one of the options above"
+
+    switch ( $choiceTeams ) {
+    
+        1 {
+
+
+
+
+        }
+    } 
+
+
+
+
+     } ##### END TEAMS
+
+
+  ######## SHAREPOINT
+
+     3 {
+
+     cls
+    Write-Host("------ SHAREPOINT---- `n") -ForegroundColor Green
+   
+    Write-Host("Select one of the following options: `n") -ForegroundColor Green
+
+    Write-Host("1.- TASK1") -ForegroundColor Yellow
+  
+    $choiceSharepoint = Read-Host -Prompt "`nSelect one of the options above"
+
+    switch ( $choiceSharepoint ) {
+    
+        1 {
+
+
 
 
      
 
+        }
     }
+
+
+
+
+     }
 }
 
 
