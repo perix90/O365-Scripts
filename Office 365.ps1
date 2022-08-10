3cd8f13e06ca023fecaef00dcd4a765ac573ca05
perix90 <pedro.gordillo.rios@icloud.com>
2022-08-10 18:14:47 +0200

    Create Office 365.ps1

---
+++ Office 365.ps1
@@ -0,0 +1,47 @@
+﻿Import-Module ExchangeOnlineManagement
+
+
+
+cls
+Write-Host("WELCOME TO OFFICE 365 MANAGEMENT V0.1 BY PEDRO GORDILLO RIOS") -ForegroundColor Green
+
+
+
+
+Write-Host("Select one of the following options:") -ForegroundColor Green
+Write-Host("")
+
+Write-Host("1.- Exchange.") -ForegroundColor Yellow
+Write-Host("2.- Teams") -ForegroundColor Yellow
+Write-Host("2.- Teams") -ForegroundColor Yellow
+
+
+
+Write-Host("")
+$choice = Read-Host -Prompt "Select one of the options above"
+
+
+switch ( $choice )
+{
+    1 {
+
+    
+
+    }
+
+    2 {
+
+    
+
+    }
+}
+
+
+Write-Host("")
+
+
+#Disconnect-ExchangeOnline -Confirm:$false -InformationAction Ignore -ErrorAction SilentlyContinue
+
+
+
+
